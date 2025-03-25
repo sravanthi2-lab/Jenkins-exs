604076f796bf75a1663c1b934cc453cb07e002fd
sravanthi2-lab <245122749301@mvsrec.edu.in>
2025-03-25 16:00:36 +0530

    Update Demo4.java

--- Demo4.java
+++ Demo4.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 class Demo4
 {
 	public static void main(String[] args){
-		int a,b;
-		Scanner sc = new Scanner(System.in); 
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]); 
 		System.out.println("Type a number:");
     a = sc.nextInt();
     System.out.println("Type another number:");
