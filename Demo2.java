c58f03d12ec62560b41697e2450d8e1deacd4b24
sravanthi2-lab <245122749301@mvsrec.edu.in>
2025-03-25 15:08:38 +0530

    Add files via upload

---
+++ Demo2.java
@@ -0,0 +1,19 @@
+class Demo2
+{
+	public static void main(String[] args){
+		int a=5,b=6;
+		int r1=a+b;
+		int r2=b-a;
+		int r3=a*b;
+		int r4=a/b;
+		int r5=a%b;
+		System.out.println(r1);
+		                System.out.println(r2);
+                System.out.println(r3);
+                System.out.println(r4);
+                System.out.println(r5);
+
+                
+
+	}
+}
