2cdef6ff8a20dcda8e4d99cc70b500df3fbd9af5
sravanthi2-lab <245122749301@mvsrec.edu.in>
2025-03-25 15:49:23 +0530

    Add files via upload

---
+++ Demo4.java
@@ -0,0 +1,26 @@
+import java.util.Scanner; 
+class Demo4
+{
+	public static void main(String[] args){
+		int a,b;
+		Scanner sc = new Scanner(System.in); 
+		System.out.println("Type a number:");
+    a = sc.nextInt();
+    System.out.println("Type another number:");
+    b = sc.nextInt();
+int r1=a+b;
+                int r2=b-a;
+                int r3=a*b;
+                int r4=a/b;
+                int r5=a%b;
+                System.out.println("Sum:"+r1);
+		System.out.println("Diff:"+r2);
+				System.out.println("Prod:"+r3);
+				System.out.println("Rem:"+r4);
+				System.out.println("Mod:"+r5);
+               
+	}
+}
+
+
+
