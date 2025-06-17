395207874cca1d379115c067b5f3f6b65e9b9797
sravanthi2-lab <245122749301@mvsrec.edu.in>
2025-06-17 16:04:12 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,14 @@
+public class Add {
+    public static void main(String[] args) {
+        // Declare and initialize two numbers
+        int num1 = 10;
+        int num2 = 20;
+        
+        // Add the two numbers
+        int sum = num1 + num2;
+        
+        // Print the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
+
