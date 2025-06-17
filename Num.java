69d15b7ee5663d448ce7ce3f51112830ddf4fa1b
sravanthi2-lab <245122749301@mvsrec.edu.in>
2025-06-17 15:54:27 +0530

    Add files via upload

---
+++ Num.java
@@ -0,0 +1,12 @@
+class Num
+{
+        public static void main(String[] args)
+        {
+                int a=2,b=3;
+                int sum=a+b;
+                double avg=(sum)/2;
+                System.out.println("Sum:" + sum);
+                System.out.println("Avg:" + avg);
+        }
+}
+~    
